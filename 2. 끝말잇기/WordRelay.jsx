d39d08b34260cefe2c51de9fecc19007579b6c38
Wonyeong Choi <coma01155@gmail.com>
2021-07-02 17:47:07 +0900

    hook으로 작성

--- 2. 끝말잇기/WordRelay.jsx
+++ 2. 끝말잇기/WordRelay.jsx
@@ -1,14 +1,40 @@
 const React = require('react');
-const {Component} = React;
+const {useState, useRef} = React;
 
-class WordRelay extends Component{
-    state={
-        text:'Hello, webpack'
+const WordRelay = () => {
+    const [word, setWord] = useState('최원영')
+    const [value, setValue] = useState('')
+    const [result, setResult] = useState('')
+
+    const inputRef = useRef(null)
+
+    const onSubmitForm = (e) => {
+        e.preventDefault()
+        if (word[word.length - 1] === value[0]) {
+            setResult('정답')
+            setWord(value)
+        } else {
+            setResult('오답')
+        }
+        setValue('')
+        inputRef.current.focus()
     }
 
-    render() {
-        return <h1>{this.state.text}</h1>
+
+    const onChange = (e) => {
+        setValue(e.target.value)
     }
+
+
+    return <>
+        <div>{word}</div>
+        <form onSubmit={onSubmitForm}>
+            <input ref={inputRef} value={value} onChange={onChange}/>
+            <button>입력!</button>
+        </form>
+        <div>{result}</div>
+    </>
+
 }
 
 module.exports = WordRelay
