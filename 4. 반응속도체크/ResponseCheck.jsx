f9f616183bacd1a087d1ddf7529a731c6faf5d86
Wonyeong Choi <coma01155@gmail.com>
2021-07-06 18:24:45 +0900

    init ResponseCheck

---
+++ 4. 반응속도체크/ResponseCheck.jsx
@@ -0,0 +1,8 @@
+const React = require('react')
+const {useRef, useState, useCallback, memo} = React;
+
+function ResponseCheck(){
+    
+}
+
+export default ResponseCheck;
---
+++ 4. 반응속도체크/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+// process.env.NODE_ENV = 'production' // 배포 모드
+
+module.exports = {
+    name: 'baseball-setting',
+
+    mode: 'development', // 실서비스: production
+
+    devtool: 'eval',// 빠르게
+
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+
+    entry: {
+        // app:['./client.jsx', './WordRelay.jsx'], 이미 client에서 WordRelay를 불렀기 때문에 호출할 필요없다.
+        app: ['./client'],
+    },// 입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            loader: 'babel-loader',
+            options: {
+                // preset은 수십개의 plugin들의 모임
+                // preset에서 option을 따로 설정할 수 있다.
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 0.5% in KR',],
+                        },
+                        debug: true,
+                    }],
+                    ['@babel/preset-react', {}],
+                ],
+                plugins:[
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ]
+            },
+        }],
+    },
+
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
+
+    output: {
+        path: path.join(__dirname, 'dist'), // 현재 폴더의 dist폴더가 path가 된다.
+        filename: 'app.js',
+        publicPath: '/dist/',
+    },// 출력
+    // 메인 설정: entry -> module -> output
+    // 추가적으로 설정할 내용은 plugins
+
+    devServer: {
+        publicPath: '/dist/',
+        hot: true, // 기존 데이터를 유지하면서 새로고침
+        port: 8089,
+    }
+}
